b6a769dd74073a2fd8d81e1e4d6a1d80998dc62d
Raquel Custodio <raquel_custodio7@hotmail.com>
2022-09-19 14:15:48 +0100

    IMC_FINAL

--- Calculator/src/calculator/IMC.java
+++ Calculator/src/calculator/IMC.java
@@ -2,17 +2,25 @@
 package calculator;
 
 
-public class IMC {
-    float imc; 
+public class IMC { 
     static void calculaIMC() {
+        float imc;
     LerDados r = new LerDados();
     r.lerValores();
-    System.out.println("IMC: "+
-     ( r.valor2 * r.valor2 / r.valor1)); 
-        if(imc =< 18.5 && imc <25);
-        System.out.println("Peso abaixo do normal"+imc);
-    
-    
-    
-    }
-    }
+
+         
+        imc = r.valor1/(r.valor2*r.valor2); 
+        if (imc <18.5) 
+            System.out.println("Abaixo do Peso"); 
+        else if (imc >= 18.5 && imc <25) 
+            System.out.println("Peso Normal"); 
+        else if (imc >=25 && imc<=29.9) 
+            System.out.println("Sobrepeso"); 
+        else if (imc >=30 && imc <=34.9) 
+            System.out.println("Obesidade Grau I"); 
+        else if (imc >=35 && imc <=39.9) 
+            System.out.println("Obesidade Grau II"); 
+        else if (imc >40) 
+            System.out.println("Obesidade Grau III"); 
+        else 
+            System.out.print("Valores inválidos");
